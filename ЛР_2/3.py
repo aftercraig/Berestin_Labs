3e1dc7fb3d9eac2040572d02af7411c61d9e8704
aftercraig <pavelboritko@gmail.com>
2024-11-29 22:17:28 +0500

    3 задача лаба 3

---
+++ ЛР_2/3.py
@@ -0,0 +1,7 @@
+a = int(input("Первое число: "))
+b = int(input("Второе число: "))
+c = int(input("Третье число: "))
+d = int(input("Четвертое число: "))
+
+max_number = max(a, b, c, d)
+print("Максимальное число:", max_number)
